c81a4add91aadd2a9bac2edb4e41ae9c4ad17946
Nguyen Duc Hoang <sunlight4d@gmail.com>
2020-04-20 13:17:53 +0700

    update

--- webcrud/product_app/views.py
+++ webcrud/product_app/views.py
@@ -3,6 +3,7 @@ from django.views.generic import View, TemplateView
 from django.http import QueryDict
 from django.http import HttpResponse
 import datetime
+import urllib.request
 
 from .models import TblProduct
 
@@ -10,28 +11,37 @@ from .models import TblProduct
 class ProductsView(TemplateView):
     http_method_names = ['get', 'post', 'put', 'delete']
     template_name = 'product_app/index.html'
+    category_id = ''
 
     def get(self, request, *args, **kwargs):        
-        category_id = request.GET['category_id']
+        self.category_id = request.GET['category_id']
         products = TblProduct.objects.filter(product_name__contains='')\
-                    .filter(category_id=category_id)\
+                    .filter(category_id=self.category_id)\
                     .order_by('product_name')        
         return render(request, self.template_name, {'products': products})
 
-    def post(self, request, *args, **kwargs):        
-        image_name = ''
+    def handle_uploaded_file(self, image_file, image_path):
+        x = os.path.dirname(os.path.realpath(__file__))
         import pdb
-        pdb.set_trace()        
+        pdb.set_trace()                    
+        with open(image_path, 'wb+') as destination:
+            for chunk in image_file.chunks():
+                destination.write(chunk)
+
+    def post(self, request, *args, **kwargs):        
+        image_name = ''        
         if request.method == 'POST' and 'product_image_file' in request.FILES:
-            image_file = request.FILES['product_image_file']
-            fs = FileSystemStorage()
-            image_name = fs.save('static/images/', image_file)
-            uploaded_file_url = fs.url(image_name)            
+            image_file = request.FILES['product_image_file']              
+            image_name = str(request.POST['product_id']) + "." + image_file.name.split('.')[-1]              
+            image_path = './images/' + image_name          
+            import pdb
+            pdb.set_trace()                      
+            self.handle_uploaded_file(image_file, image_path)            
         new_product = TblProduct(product_id = int(request.POST['product_id']), \
-                                 product_name = request.POST['product_name'],
-                                 available = request.POST['product_available'],
-                                 image_name = image_name,
-                                 )
+                                 product_name = request.POST['product_name'],\
+                                 available = request.POST['product_available'],\
+                                 image_name = image_name,\
+                                 category_id = self.category_id)
         new_product.save()
         return self.get(request, self.template_name)
 
