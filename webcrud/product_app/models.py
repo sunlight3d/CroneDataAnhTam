f912dccd28c423376e7d35d09540fa7c973dd0cc
Nguyen Duc Hoang <sunlight4d@gmail.com>
2020-04-21 16:34:47 +0700

    update

--- webcrud/product_app/models.py
+++ webcrud/product_app/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 import re
 import os
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.contrib.staticfiles import finders
 
 class TblProduct(models.Model):
     product_id = models.CharField(primary_key=True, blank=True, null=False, max_length=2000)
@@ -11,13 +13,13 @@ class TblProduct(models.Model):
     category_id = models.IntegerField(blank=True, null=True)
 
     @property
-    def get_image_name(self):
-    	my_image_name = self.product_id + ".jpg"
-    	my_image_name = re.sub(r'\s+', '', my_image_name)
-    	my_image_name = re.sub(r'/', '-', my_image_name)    	
-    	if os.path.isfile('../images/' + my_image_name) == False:
-    		return 'noimage.jpg'
-    	return my_image_name
+    def get_image_name(self):                
+        my_image_name = self.product_id + ".jpg"
+        my_image_name = re.sub(r'\s+', '', my_image_name)
+        my_image_name = re.sub(r'/', '-', my_image_name)        
+        if finders.find('images/'+my_image_name) is None:
+            return 'noimage.jpg'
+        return my_image_name
     
     class Meta:
         managed = False
