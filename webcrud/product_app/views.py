3fe4872e26a138404c6473caa84abe96e5a2b4f1
Nguyen Duc Hoang <sunlight4d@gmail.com>
2020-04-22 12:20:25 +0700

    update

--- webcrud/product_app/views.py
+++ webcrud/product_app/views.py
@@ -26,19 +26,31 @@ class ProductsView(TemplateView):
             destination.close()
 
     def post(self, request, *args, **kwargs):        
-        image_name = ''        
+        image_name = ''           
         if request.method == 'POST' and 'product_image_file' in request.FILES:
             image_file = request.FILES['product_image_file']              
             # image_name = str(request.POST['product_id']) + "." + image_file.name.split('.')[-1]              
             image_name = str(request.POST['product_id']) + ".jpg"
             image_path = os.path.join(os.path.abspath('.'), 'static/images/' + image_name)                
             self.handle_uploaded_file(image_file, image_path)            
-        new_product = TblProduct(product_id = request.POST['product_id'], \
-                                 product_name = request.POST['product_name'],\
-                                 available = request.POST['product_available'],\
-                                 image_name = image_name,\
-                                 category_id = int(request.GET['category_id']))
-        new_product.save()
+        if request.POST['type'] == 'insert':            
+            new_product = TblProduct(product_id = request.POST['product_id'], \
+                                     product_name = request.POST['product_name'],\
+                                     available = request.POST['product_available'],\
+                                     image_name = image_name,\
+                                     category_id = int(request.GET['category_id']))
+            new_product.save()
+        elif request.POST['type'] == 'update':            
+            selected_product = TblProduct.objects.get(product_id=request.POST['product_id'])            
+            if selected_product is not None:
+                selected_product.product_name = request.POST['product_name']        
+                selected_product.available = request.POST['product_available']  
+                if image_name.strip() != '' and selected_product.image_name.strip() != image_name.strip():                
+                    image_path = os.path.join(os.path.abspath('.'), 'static/images/' + selected_product.image_name)                      
+                    if os.path.exists(image_path):
+                        os.remove(image_path)
+                    selected_product.image_name = image_name       
+                selected_product.save()
         return self.get(request, self.template_name)
 
     def delete(self, request, *args, **kwargs):
@@ -46,22 +58,10 @@ class ProductsView(TemplateView):
         product_id = int(params.get('product_id'))
         product_name = params.get('product_name')
         selected_product = TblProduct.objects.get(product_id=product_id)                
-        image_name = str(selected_product.product_id) + ".jpg"
-        import pdb
-        pdb.set_trace()
+        image_name = str(selected_product.product_id) + ".jpg"        
         if image_name != '':
             image_path = os.path.join(os.path.abspath('.'), 'static/images/' + image_name)  
             if os.path.exists(image_path):
                 os.remove(image_path)
         selected_product.delete()
-        return self.get(request, args, kwargs)
-
-    def put(self, request, *args, **kwargs):
-        params = QueryDict(request.body)
-        import pdb
-        pdb.set_trace()
-        product_id = params.get('product_id')
-        product_name = params.get('product_name')
-        selected_product = TblProduct.objects.get(product_id=product_id)
-        selected_product.product_name = product_name        
-        return self.get(request, self.template_name)
+        return self.get(request, args, kwargs)    
